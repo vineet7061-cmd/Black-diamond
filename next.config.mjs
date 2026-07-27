563aecf162526683093f5a3f109121c391bbcdfb
shinchanisnew3115-jpg <shinchanisnew3115@gmail.com>
2026-07-27 22:55:13 +0530

    Disabled Webpack cache to fix OOM crash

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", 
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -18,10 +11,9 @@ const nextConfig = {
   },
   productionBrowserSourceMaps: false,
   experimental: {
-    // Ye do lines Vercel ki RAM bachayengi
-    cpus: 1, 
+    cpus: 1,
     workerThreads: false,
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
